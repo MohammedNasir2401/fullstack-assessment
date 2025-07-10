721fe10e7086ff1360db74397ff4ca5e278e83a0
Moh <moh@mo.mo>
2025-07-10 17:22:09 +0400

    Fix: Prisma Client Err

--- api/src/scripts/insert-records.js
+++ api/src/scripts/insert-records.js
@@ -1,4 +1,5 @@
-import prisma from '../../prisma/prismaClient.js';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 import { readDataFromFile } from '../utils/reports.js';
 
 
